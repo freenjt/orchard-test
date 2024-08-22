19745a1cbdca2f50463a8e7fc1a455c9d8783ffd
freenjt <freenjt@hotmail.com>
2024-08-22 11:59:39 -0500

    Bootstrap theme and styles changes

--- web/themes/custom/orchard_bootstrap/js/global.js
+++ web/themes/custom/orchard_bootstrap/js/global.js
@@ -9,8 +9,15 @@
 
   Drupal.behaviors.orchard_bootstrap = {
     attach: function (context, settings) {
-
-    }
+      let bannerNavItem = document.querySelectorAll('.menu--banner-navigation .dropdown-item');
+      bannerNavItem.forEach((item) => {
+        item.addEventListener('click', (event) => {
+          event.preventDefault();
+          const imageUrl = event.target.getAttribute('data-banner');
+          document.getElementById('banner').style.backgroundImage = `url('${imageUrl}')`;
+        });
+      });
+    },
   };
 
 })(Drupal);
